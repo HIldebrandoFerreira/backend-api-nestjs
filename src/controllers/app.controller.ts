59111cc15fd55470f6ee2088dcb84f1e2f935642
Hildebrando Ferreira Nascimento <A0091706@redecorp.br>
2020-12-09 20:22:43 -0300

    Teste de commite

---
+++ src/controllers/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from '../service/app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/projects')
+  getAllRepositories(): Array<string> {
+    return this.appService.getAllRepositories();
+  }
+}
